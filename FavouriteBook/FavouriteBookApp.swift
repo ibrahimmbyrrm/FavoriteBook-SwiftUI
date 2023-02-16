88acab7cef1c4d787fe02d2a565e7c508691ddd9
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-02-16 19:21:01 +0300

    Initial Commit

---
+++ FavouriteBook/FavouriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteBookApp.swift
+//  FavouriteBook
+//
+//  Created by İbrahim Bayram on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
